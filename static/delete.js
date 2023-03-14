8ac12daa7a0b4d47a8476d40bc1317def2995672
Henry Gerald Murdock <107006226+Murdock022X@users.noreply.github.com>
2023-03-14 00:19:39 -0400

    Delete static directory

--- static/delete.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    (document.querySelectorAll('.notification .delete')).forEach(($delete) => {
-        const $notification = $delete.parentNode
-
-        $delete.addEventListener('click', () => {
-            $notification.parentNode.removeChild($notification)
-        });
-    });
-});
--- static/nav.js
+++
@@ -1,8 +0,0 @@
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click', 
-    () => {navbarMenu.classList.toggle('is-active')})
-
-burgerIcon.addEventListener('click', 
-    () => {burgerIcon.classList.toggle('is-active')})
